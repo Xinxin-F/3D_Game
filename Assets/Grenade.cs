8610af8025e7c871a2d60d82dc7dfb6e172fe93a
Xinxin F <fanxinxin@outlook.com>
2024-05-18 14:58:10 +0800

    adjust code for grenade. Can explode once

--- Assets/Grenade.cs
+++ Assets/Grenade.cs
@@ -4,64 +4,131 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float lifetime = 7f;
-    [SerializeField] private float detonationTime = 3f; 
-
-    private Rigidbody rb;
+    public float delay = 3f;
+    float countdown;
+    [SerializeField] private GameObject ExplosionEffect;
+    bool hasExploded = false;
     [SerializeField] private float grenadeDamage = 5f;
-    private bool hasDetonated = false;
-    [SerializeField] private GameObject grenadeExplosion;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        Destroy(gameObject, lifetime);
+    [SerializeField] private float radius = 2f;
+    
+    void Start(){
+        countdown = delay;
     }
 
-    public void SetDirection(Vector3 direction)
-    {
-        rb.velocity = direction * speed;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Boss"))
-        {
-            Detonate();
-            // Deal damage or other effects to the enemy
-            other.GetComponent<HealthController>().TakeDamage(grenadeDamage);
-        }
-        else if (!hasDetonated)
-        {
-            StartCoroutine(DetonationTimer());
+    void Update(){
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded){
+            Exploded();
+            hasExploded = true;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!hasDetonated)
-        {
-            StartCoroutine(DetonationTimer());
+    void Exploded(){
+
+        //show visual effect
+        //  Instantiate(ExplosionEffect, transform.position, transform.rotation);
+
+        GameObject explosionInstance = Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        ParticleSystem ps = explosionInstance.GetComponent<ParticleSystem>();
+
+            if (ps != null)
+            {
+                ps.Play();
+                //Debug.Log("Playing particle system");
+                Destroy(explosionInstance, ps.main.duration);
+            }
+            else
+            {
+                Debug.LogWarning("Particle system component not found on the prefab");
+            }
+        
+        //get nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders){
+            if (nearbyObject.gameObject.CompareTag("Boss"))
+            {
+                nearbyObject.GetComponent<HealthController>().TakeDamage(grenadeDamage);
+            }
         }
-    }
-
-    private IEnumerator DetonationTimer()
-    {
-        hasDetonated = true;
-        yield return new WaitForSeconds(detonationTime);
-        Detonate();
-    }
-
-    private void Detonate()
-    {
-        // Implement grenade explosion effects or damage here
-        Instantiate(grenadeExplosion, transform.position, transform.rotation);
+        //damage
 
         Destroy(gameObject);
+
+
+        
     }
+
+
+
+
+
+}
+
+
+
+    // [SerializeField] private float speed = 10f;
+    // [SerializeField] private float lifetime = 7f;
+    // [SerializeField] private float detonationTime = 3f; 
+
+    // private Rigidbody rb;
+    // [SerializeField] private float grenadeDamage = 5f;
+    // private bool hasDetonated = false;
+    // [SerializeField] private GameObject grenadeExplosion;
+
+    // private void Start()
+    // {
+    //     rb = GetComponent<Rigidbody>();
+    //     Destroy(gameObject, lifetime);
+    // }
+
+    // public void SetDirection(Vector3 direction)
+    // {
+    //     rb.velocity = direction * speed;
+    // }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     // if (other.gameObject.CompareTag("Boss"))
+    //     // {
+    //     //     //Detonate();
+    //     //     // Deal damage or other effects to the enemy
+    //     //     other.GetComponent<HealthController>().TakeDamage(grenadeDamage);
+    //     // }
+    //     // else if (!hasDetonated)
+    //     // {
+    //     //     StartCoroutine(DetonationTimer());
+    //     // }
+    //     StartCoroutine(DetonationTimer());
+
+    // }
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (!hasDetonated)
+    //     {
+    //         StartCoroutine(DetonationTimer());
+    //     }
+    // }
+
+    // private IEnumerator DetonationTimer()
+    // {
+    //     hasDetonated = true;
+    //     yield return new WaitForSeconds(detonationTime);
+    //     Detonate();
+    // }
+
+    // private void Detonate()
+    // {
+    //     // Implement grenade explosion effects or damage here
+    //     Instantiate(grenadeExplosion, transform.position, transform.rotation);
+    //     Destroy(gameObject);
+        
+    // }
+
+
 
 
 
     
-}
+
