e243d6b08fb3105bc1f61b35933e2f8f3dce94ca
Xinxin F <fanxinxin@outlook.com>
2024-05-22 11:13:25 +0800

    spawned obstacles success

---
+++ Assets/Spawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class SpawnableObject
+    {
+        public GameObject prefab;
+        public int spawnNumber;
+    }
+
+    public List<SpawnableObject> spawnPool;
+    public GameObject spawnArea;
+
+    void Start()
+    {
+        foreach (var spawnObject in spawnPool)
+        {
+            SpawnObjects(spawnObject.prefab, spawnObject.spawnNumber);
+        }
+    }
+
+    public float buffer = 1.0f;
+    public void SpawnObjects(GameObject objectToSpawn, int numToSpawn)
+    {
+        Bounds bounds = spawnArea.GetComponent<Renderer>().bounds;
+
+        for (int i = 0; i < numToSpawn; i++)
+        {
+            // Vector3 randomPosition = new Vector3(Random.Range(bounds.min.x, bounds.max.x),
+            //                                      objectToSpawn.transform.position.y,
+            //                                      Random.Range(bounds.min.z, bounds.max.z));
+
+            Vector3 randomPosition = new Vector3(Random.Range(bounds.min.x + buffer, bounds.max.x - buffer),
+                                     objectToSpawn.transform.position.y,
+                                     Random.Range(bounds.min.z + buffer, bounds.max.z - buffer));
+
+            randomPosition.y = GetObstacleHeight(randomPosition) + objectToSpawn.transform.localScale.y / 2;
+
+            if (!IsOverlapping(randomPosition, objectToSpawn.transform.localScale.x))
+            {
+                Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private float GetObstacleHeight(Vector3 position)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
+        {
+            return hit.point.y;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
+    private bool IsOverlapping(Vector3 position, float objectSize)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, objectSize / 2);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject != spawnArea)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
