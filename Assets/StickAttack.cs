6aa79921c8ceaaa4284c48d2b9a65994fc0d97f6
Xinxin F <fanxinxin@outlook.com>
2024-05-23 15:31:56 +0800

    self destroyed okay. no Sticking

--- Assets/StickAttack.cs
+++ Assets/StickAttack.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class StickAttack : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float waterSpeed = 10f;
 
-    //[SerializeField] private GameObject assistant;
+    
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,26 +16,24 @@ public class StickAttack : MonoBehaviour
             Debug.Log("OnCollisionEnter triggered with Ground");
             Vector3 BossPosition = Vector3.zero;
             RotateTowards(BossPosition);
-            ShootAtBoss(BossPosition);
+            InvokeRepeating("ShootAtBoss", 0f, 3f);
+            Destroy(gameObject, 10f);
         }
     }
 
-     private void RotateTowards(Vector3 targetPoint)
+    private void RotateTowards(Vector3 targetPoint)
     {
         Vector3 direction = (targetPoint - transform.position).normalized;
         direction.y = 0;
         transform.rotation = Quaternion.LookRotation(direction);
     }
 
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform bulletSpawnPoint;
-    [SerializeField] private float waterSpeed = 10f;
-
-    private void ShootAtBoss(Vector3 targetPosition)
+    private void ShootAtBoss()
     {
         Debug.Log("ShootAtBoss called");
+        Vector3 BossPosition = Vector3.zero;
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Vector3 direction = targetPosition - bulletSpawnPoint.position;
+        Vector3 direction = BossPosition - bulletSpawnPoint.position;
         bullet.GetComponent<Rigidbody>().velocity = direction.normalized * waterSpeed;
     }
 }
