738723977a57f6d00e99c66212fab6725a909973
Xinxin F <fanxinxin@outlook.com>
2024-05-25 08:43:20 +0800

    archive

--- Assets/Scripts/Skill/HealAbility.cs
+++ Assets/Scripts/Skill/HealAbility.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 public class HealAbility : Ability
 {
     public float healAmount;
+    [SerializeField] private ParticleSystem healParticleSystem;
 
     public override void Activate(GameObject parent) {
         HealthController health = parent.GetComponent<HealthController>();
         if (health != null) {
             health.Heal(healAmount);
+            PlayParticle(parent);
+        }
+    }
+
+    private void PlayParticle(GameObject parent)
+    {
+        if (healParticleSystem != null)
+        {
+            ParticleSystem instance = Instantiate(healParticleSystem, parent.transform.position, Quaternion.identity);
+            instance.Play();
+            Destroy(instance.gameObject, 3f); // Destroy after 3 seconds
+        }
+        else
+        {
+            Debug.LogWarning("Particle system not assigned");
         }
     }
 }
