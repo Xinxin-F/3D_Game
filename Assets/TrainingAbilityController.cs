df001d3daa82ecfb9a915435ff2699dc4076d1f8
Xinxin F <fanxinxin@outlook.com>
2024-05-26 09:29:35 +0800

    can test elemental skills in training scene

--- Assets/TrainingAbilityController.cs
+++ Assets/TrainingAbilityController.cs
@@ -23,14 +23,44 @@ public class TrainingAbilityController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            selectedAbility?.Activate(gameObject);
-        }
+        HandleAbilityActivation();
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     selectedAbility?.Activate(gameObject);
+        // }
+        
     }
 
     void SelectAbility(int index)
     {
         selectedAbility = abilities[index];
+    }
+
+
+    private float pressTime = 0f;
+    private float longPressThreshold = 0.5f;
+
+    private void HandleAbilityActivation()
+    {
+        if (Input.GetMouseButtonDown(0))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        {
+            pressTime = Time.time;
+        }
+        
+        if (Input.GetMouseButtonUp(0) && selectedAbility != null)
+        {
+            float pressDuration = Time.time - pressTime;
+            bool isLongPress = pressDuration >= longPressThreshold;
+
+            //short press for fire, long press for ice
+            if (selectedAbility is ElementalReactionSkill)
+            {
+                ((ElementalReactionSkill)selectedAbility).Activate(gameObject, isLongPress);
+            }
+            else
+            {
+                selectedAbility?.Activate(gameObject);
+            }
+        }
     }
 }
